eba491e9a53e812479d451800f3e233ac68831ab
Chu Hyeonjin <me@hyeonjin.kr>
2023-02-05 01:10:14 +0900

    feat: make / page to profile page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>추현진</h1>
+<h3>Chu Hyeonjin</h3>
+<a href="https://github.com/chuhyeonjin/">Github</a>
+
+<svelte:head>
+  <title>추현진</title>
+</svelte:head>
